c7a7d43bd592b35116c4a9eaf6b54907b91ee395
André Vitória <andre_davitoria@hotmail.com>
2018-09-23 15:28:57 -0300

    Inserçao do Materialize e criação do template base

--- E_Vento/views.py
+++ E_Vento/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    temp = loader.get_template('evento_site/pages/home.html')
+    return HttpResponse(temp.render({}, request))
